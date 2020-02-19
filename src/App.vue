46ec480b99d2f404abf9a82b958710f0e84ed380
AnatolyLevchenko <kypiwindy@xaker.ru>
2020-02-19 15:34:39 +0200

    fix bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
     <div>
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#" class="brand">Simple Ecommerce</b-navbar-brand>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#" class="brand">Simple E-commerce</b-navbar-brand>
+
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="m-auto">
+            <b-nav-item to="/" exact exact-active-class="active">Home</b-nav-item>
+            <b-nav-item href="#">Categories</b-nav-item>
+            <b-nav-item href="#">Product</b-nav-item>
+            <b-nav-item to="/cart/" exact exact-active-class="active">Cart</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="my-2 my-lg-0">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
       </b-navbar>
       <router-view></router-view>
     </div>
